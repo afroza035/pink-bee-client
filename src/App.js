6491363bea2385d0d9fb15dd1feb68495a562270
Afroza Akter Ruma <afroza.akter.ruma035@gmail.com>
2021-06-23 12:50:41 +0600

    react-router i and bootstrap set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route 
+} from "react-router-dom";
+
+import NotFound from "./components/NotFound/NotFound";
+import Home from "./components/Home/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>PinkBee</h1>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
